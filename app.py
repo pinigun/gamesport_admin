7d8775bd9a17f1c2d866a1f92032121127638a5b
pinigun <piniguun@yandex.ru>
2025-04-22 13:36:18 +0500

    Fix: add cors midleware

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from api.routers import api_router
 
@@ -8,6 +9,14 @@ api = FastAPI()
 api.include_router(api_router)
 
 app.mount('/admin_panel', api, "API")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["Authorization"]
+)
 
 if __name__ == '__main__':
     uvicorn.run(app)
