e2ac5db2fa2c9ffca3b8d574fabcabc20a312c05
pinigun <piniguun@yandex.ru>
2025-04-22 15:06:29 +0500

    disable authorization

--- api/routers/__init__.py
+++ api/routers/__init__.py
@@ -4,7 +4,7 @@ from .auth.routes import router as auth_router
 from .users.routes import router as users_router
 from .admins.routes import router as admin_router
 from .faq.routes import router as faq_router
-from .statistics.routes import router as statistics_router
+# from .statistics.routes import router as statistics_router
 
 api_router = APIRouter()
 
@@ -12,4 +12,4 @@ api_router.include_router(auth_router)
 api_router.include_router(admin_router)
 api_router.include_router(users_router)
 api_router.include_router(faq_router)
-api_router.include_router(statistics_router)
+# api_router.include_router(statistics_router)
