997ba84ccaf5ab074c407e6689be5f44e240902d
pinigun <piniguun@yandex.ru>
2025-04-28 05:03:05 +0500

    Feature: add giveaways setters

---
+++ tools/photos.py
@@ -0,0 +1,11 @@
+from fastapi import UploadFile
+from filelock import AsyncFileLock
+import aiofiles
+
+class PhotoTools:
+    async def save_photo(path: str, photo: UploadFile):
+        file_path = f'{path}/"general"{photo.filename.split('.')[-1]}'
+        async with AsyncFileLock(f'{file_path}.lock', timeout=10):
+            async with aiofiles.open(file_path, 'wb') as f:
+                await f.write(await photo.read())
+                return file_path
